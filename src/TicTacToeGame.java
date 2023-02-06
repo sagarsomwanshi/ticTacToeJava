80b2834f110f67d58e6d31bf4abeaee1481076b9
sagarsomwanshi <sagarsomwanshi13@gmail.com>
2023-02-06 18:00:00 +0530

    Creates board & assigns block number

---
+++ src/TicTacToeGame.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class TicTacToeGame {
+    public static void main(String[] args) {
+        UC1();
+
+    }
+    public static void UC1(){
+        String [] board = new String[10];
+        for(int i = 1; i< board.length; i++){
+            board[i] = String.valueOf(i);
+        }
+    }
+}
