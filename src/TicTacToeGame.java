eb865bc3b4e3f254cebc6926035a649f6fe7c2e2
sagarsomwanshi <sagarsomwanshi13@gmail.com>
2023-02-06 20:08:17 +0530

    Gives ability to user to select slot & make move

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -2,10 +2,11 @@ import java.util.*;
 public class TicTacToeGame {
     static Scanner sc = new Scanner(System.in);
     static String [] board = new String[10];
+    static String playLetter = null;
     public static void main(String[] args) {
         UC1();
         UC2();
-        UC3ShowBoard();
+        UC4();
 
     }
     public static void UC1(){
@@ -16,7 +17,7 @@ public class TicTacToeGame {
     }
     public static void UC2(){
         System.out.println("Please choose your letter for playing (X or O) \ntype letter & press enter");
-        String playLetter = sc.next();
+        playLetter = sc.next();
         if(playLetter.equals("x") ){
             System.out.println("You choose X \nnow computer will play with O");
         } else if(playLetter.equals("o")){
@@ -31,5 +32,25 @@ public class TicTacToeGame {
         System.out.println("| " +board[1] + " | " +board[2]+ " | " +board[3]+ " |\n-------------");
         System.out.println("| " +board[4] + " | " +board[5]+ " | " +board[6]+ " |\n-------------");
         System.out.println("| " +board[7] + " | " +board[8]+ " | " +board[9]+ " |\n-------------");
+    }
+    public static void UC4(){
+        System.out.println("");
+        UC3ShowBoard();
+        System.out.println("Enter a slot number to Enter "+playLetter);
+        int input = sc.nextInt();
+
+            switch (input) {
+                case 1, 2, 3, 4, 5, 6, 7, 8, 9:
+                    board[input] = playLetter;
+                    UC3ShowBoard();
+                    break;
+                default:
+                    System.out.println("Invalid slot Number. please Enter valid slot Number");
+                    UC4();
+                    break;
+
+            }
+
+
     }
 }
